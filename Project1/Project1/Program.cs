30bd2eb8dd9bf0cfdb7a545a78572b1a695ac18d
Mitchellvr <474582847@qq.com>
2017-05-31 16:31:20 +0800

    Add files via upload

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxR_2
+{
+    class Program
+    {
+        private int count = 1;
+        private double sum = 0;
+        static void Main(string[] args)
+        {
+         
+        }
+
+        double func(int m)
+        {
+            if (count == m)
+                return sum;
+            FindMaxCircle();
+            count++;
+            func(m);
+            return 0;
+        }
+    }
+}
