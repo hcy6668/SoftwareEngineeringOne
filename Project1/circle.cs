f04f001c49a56f20ee3f0ba3b0acf4c533cbbd40
Mitchellvr <474582847@qq.com>
2017-05-31 16:28:29 +0800

    Add files via upload

---
+++ Project1/circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxR_2
+{
+    class circle
+    {
+        private float x;
+        private float y;
+        private float r;
+        private double SumPart;
+    }
+}
